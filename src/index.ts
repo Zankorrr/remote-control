12534ff6b343736d9b43914ea25784ce5d1cf90f
Zankorrr <zankor92@gmail.com>
2023-01-21 16:54:20 +0100

    feat: create ws server, add drawSquare function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
+import { WebSocketServer } from 'ws';
 import httpServer from "./http_server";
-import { mouse } from "@nut-tree/nut-js";
+import drawSquare from './square';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    const command = data.toString().split(' ')[0]
+    const size = +data.toString().split(' ')[1]
+    switch (command) {
+      case "draw_square":
+        drawSquare(size)
+        break;
+      default:
+        break;
+    }
+  });
+});
