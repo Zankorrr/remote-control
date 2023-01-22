531e65c841b91e34a71062cfeceec04e6e2422ae
Zankorrr <zankor92@gmail.com>
2023-01-22 12:35:13 +0100

    fix: remove mousePosition function

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import drawCircle from './circle';
 import httpServer from "./http_server";
 import mouseDown from './mouseDown';
 import mouseLeft from './mouseLeft';
-import mousePosition from './mousePosition';
 import mouseRight from './mouseRight';
 import mouseUp from './mouseUp';
 import drawRectangle from './rectangle';
@@ -17,34 +16,38 @@ httpServer.listen(HTTP_PORT);
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
+  ws.on('message', async function message(data) {
     console.log('received: %s', data);
     const command = data.toString().split(' ')[0]
     const size = +data.toString().split(' ')[1]
     const length = +data.toString().split(' ')[2]
     switch (command) {
       case "mouse_up":
+        ws.send(`${command}:${size}`)
         mouseUp(size)
         break
       case "mouse_right":
+        ws.send(`${command}:${size}`)
         mouseRight(size)
         break
       case "mouse_down":
+        ws.send(`${command}:${size}`)
         mouseDown(size)
         break
       case "mouse_left":
+        ws.send(`${command}:${size}`)
         mouseLeft(size)
         break
-      case "mouse_position":
-          mousePosition()
-          break
       case "draw_square":
+        ws.send(`${command}:${size}`)
         drawSquare(size)
         break;
       case "draw_rectangle":
+        ws.send(`${command}:${size}x${length}`)
         drawRectangle(size, length)
         break
       case "draw_circle":
+        ws.send(`${command}:${size}`)
         drawCircle(size)
         break
       default:
--- src/mousePosition.ts
+++
@@ -1,7 +0,0 @@
-import { mouse } from "@nut-tree/nut-js";
-
-const mousePosition = async () => {
-  console.log(await mouse.getPosition())
-}
-
-export default mousePosition
